542c34e872cf21ac606d0065af8dd12d35564e5b
Renato Vicente <rntvicente@gmail.com>
2021-04-26 21:35:54 -0300

    aplicando validação de httpRequest

--- test/presentation/login-router.test.js
+++ test/presentation/login-router.test.js
@@ -2,6 +2,12 @@ const { assert } = require('chai')
 
 class LoginRouter {
   async route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { body: { emai, password } } = httpRequest
 
     if (!emai || !password) {
@@ -35,5 +41,20 @@ describe('Login Router', () => {
 
     const httpResponse = await sut.route(httpResquest)
     assert.equal(httpResponse.statusCode, 400)
+  })
+
+  it('should return 500 when no httpRequest is provided', async () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = await sut.route()
+    assert.equal(httpResponse.statusCode, 500)
+  })
+
+  it('should return 500 when httpRequest has no body', async () => {
+    const sut = new LoginRouter()
+    const httpResquest = {}
+
+    const httpResponse = await sut.route(httpResquest)
+    assert.equal(httpResponse.statusCode, 500)
   })
 })
