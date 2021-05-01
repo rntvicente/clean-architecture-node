dcaa98916008c2ca171701c34b4d0ff1ec68641b
Renato Vicente <rntvicente@gmail.com>
2021-05-01 14:52:16 -0300

    validando existencia de user

---
+++ src/domain/usescases/auth-usecase.js
@@ -0,0 +1,32 @@
+const { MissingParamError, InvalidParamError } = require('../../../src/utils/errors')
+module.exports = class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
+  async auth (email, password) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+
+    if (!this.loadUserByEmailRepository) {
+      throw new MissingParamError('loadUserByEmailRepository')
+    }
+
+    if (!this.loadUserByEmailRepository.load) {
+      throw new InvalidParamError('loadUserByEmailRepository')
+    }
+
+    const user = await this.loadUserByEmailRepository.load(email)
+
+    if (!user) {
+      return null
+    }
+
+    return null
+  }
+}
--- test/domain/usecases/auth-usecases.test.js
+++ test/domain/usecases/auth-usecases.test.js
@@ -3,42 +3,19 @@ const chaiAsPromised = require('chai-as-promised')
 const sinon = require('sinon')
 chai.use(chaiAsPromised)
 
-const { MissingParamError, InvalidParamError } = require('../../../src/utils/errors')
-
-class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-  }
-
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
-    }
-
-    if (!password) {
-      throw new MissingParamError('password')
-    }
-
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository')
-    }
-
-    if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamError('loadUserByEmailRepository')
-    }
-
-    await this.loadUserByEmailRepository.load(email)
-  }
-}
+const AuthUseCase = require('../../../src/domain/usescases/auth-usecase')
 
 const makeSut = () => {
   class LoadUserByEmailRepository {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
 
   const loadUserByEmailRepository = new LoadUserByEmailRepository()
+  loadUserByEmailRepository.user = {}
+
   const sut = new AuthUseCase(loadUserByEmailRepository)
 
   return {
@@ -49,7 +26,7 @@ const makeSut = () => {
 
 const sandbox = sinon.createSandbox()
 
-describe.only('Auth Usecase', () => {
+describe('Auth Usecase', () => {
   afterEach(() => sandbox.restore())
 
   it('should return throws when no email is provided', async () => {
@@ -78,5 +55,20 @@ describe.only('Auth Usecase', () => {
   it('should throw when LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
     chai.assert.isRejected(sut.auth('any_email@email.com', 'any_password'))
+  })
+
+  it('should null when an invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepository } = makeSut()
+    loadUserByEmailRepository.user = null
+
+    const accessToken = await sut.auth('invalid_email@email.com', 'any_password')
+    chai.assert.isNull(accessToken)
+  })
+
+  it('should null when an invalid password is provided', async () => {
+    const { sut } = makeSut()
+
+    const accessToken = await sut.auth('valid_email@email.com', 'invalid_password')
+    chai.assert.isNull(accessToken)
   })
 })
