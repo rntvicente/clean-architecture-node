29f61275a2eee51618d3c8819747e6fa82cb0b98
Renato Vicente <rntvicente@gmail.com>
2021-05-01 14:07:17 -0300

    validando LoadUserByEmailRepository está recebendo o email esperdo

--- test/domain/usecases/auth-usecases.test.js
+++ test/domain/usecases/auth-usecases.test.js
@@ -1,10 +1,15 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-
-const { MissingParamError } = require('../../../src/utils/errors')
+const sinon = require('sinon')
 chai.use(chaiAsPromised)
 
+const { MissingParamError } = require('../../../src/utils/errors')
+
 class AuthUseCase {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -13,10 +18,16 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
+const sandbox = sinon.createSandbox()
+
 describe('Auth Usecase', () => {
+  afterEach(() => sandbox.restore())
+
   it('should return throws when no email is provided', async () => {
     const sut = new AuthUseCase()
 
@@ -27,5 +38,21 @@ describe('Auth Usecase', () => {
     const sut = new AuthUseCase()
 
     return chai.assert.isRejected(sut.auth('any_email@email.com'))
+  })
+
+  it('should call LoadUserByEmailRepository with email correct', async () => {
+    class LoadUserByEmailRepository {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailRepository = new LoadUserByEmailRepository()
+    const sut = await new AuthUseCase(loadUserByEmailRepository)
+
+    const loadUserByEmailRepositorySpy = sandbox.spy(loadUserByEmailRepository, 'load')
+    sut.auth('any_email@email.com', 'any_password')
+
+    chai.assert.isTrue(loadUserByEmailRepositorySpy.calledOnceWith('any_email@email.com'))
   })
 })
