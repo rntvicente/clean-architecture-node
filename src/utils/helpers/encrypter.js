7b2c1027cc08de453bda05e97d2d188c82c07dc1
Renato Vicente <rntvicente@gmail.com>
2021-05-01 20:36:17 -0300

    validação de parametros invalidos Encrypter

--- src/utils/helpers/encrypter.js
+++ src/utils/helpers/encrypter.js
@@ -1,6 +1,16 @@
 const bcrypt = require('bcrypt')
+
+const { MissingParamError } = require('../errors')
 module.exports = class Encrypter {
   async compare (value, hash) {
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+
+    if (!hash) {
+      throw new MissingParamError('hash')
+    }
+
     const isValid = await bcrypt.compare(value, hash)
     return isValid
   }
