718262381a756275309012d0e8d40fb45498a592
Renato Vicente <rntvicente@gmail.com>
2021-05-01 21:43:06 -0300

    validação de gerar excessão quando não informar parametros obrigatórios

--- src/utils/helpers/token-generator.js
+++ src/utils/helpers/token-generator.js
@@ -1,11 +1,21 @@
 const jwt = require('jsonwebtoken')
 
+const { MissingParamError } = require('../errors')
+
 module.exports = class TokenGenerator {
   constructor (secret) {
     this.secret = secret
   }
 
   async generate (id) {
+    if (!this.secret) {
+      throw MissingParamError('secret')
+    }
+
+    if (!id) {
+      throw MissingParamError('id')
+    }
+
     return jwt.sign(id, this.secret)
   }
 }
--- test/utils/token-generator.test.js
+++ test/utils/token-generator.test.js
@@ -1,14 +1,17 @@
 const jwt = require('jsonwebtoken')
+const chaiAsPromised = require('chai-as-promised')
 const sinon = require('sinon')
-const { assert } = require('chai')
+const chai = require('chai')
 
 const Encrypter = require('../../src/utils/helpers/token-generator')
+const TokenGenerator = require('../../src/utils/helpers/token-generator')
 
 const makeSut = () => {
   return new Encrypter('secret')
 }
 
 const sandbox = sinon.createSandbox()
+chai.use(chaiAsPromised)
 
 describe('Token Generator', () => {
   afterEach(() => sandbox.restore())
@@ -19,7 +22,7 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
 
-    assert.isNull(token)
+    chai.assert.isNull(token)
   })
 
   it('should return a token when JWT returns token', async () => {
@@ -28,7 +31,7 @@ describe('Token Generator', () => {
     const sut = makeSut()
     const isValid = await sut.generate('any_id')
 
-    assert.strictEqual(isValid, 'valid_token')
+    chai.assert.strictEqual(isValid, 'valid_token')
   })
 
   it('should call JWT with correct values', async () => {
@@ -37,6 +40,16 @@ describe('Token Generator', () => {
     const sut = makeSut()
     await sut.generate('any_id')
 
-    assert.isTrue(signSpy.calledOnceWith('any_id', sut.secret))
+    chai.assert.isTrue(signSpy.calledOnceWith('any_id', sut.secret))
+  })
+
+  it('should return throw when no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    chai.assert.isRejected(sut.generate('any_id'))
+  })
+
+  it('should return throw when no id is provided', async () => {
+    const sut = makeSut()
+    chai.assert.isRejected(sut.generate())
   })
 })
