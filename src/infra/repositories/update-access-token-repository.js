7cd2948070b602099ffd6736746fbb9f51288136
Renato Vicente <rntvicente@gmail.com>
2021-05-02 20:15:27 -0300

    retornando erro personalisado na metodo UpdateAccessTokenRepository quando não passar campos obrigatórios

--- src/infra/repositories/update-access-token-repository.js
+++ src/infra/repositories/update-access-token-repository.js
@@ -1,9 +1,18 @@
+const { MissingParamError } = require('../../utils/errors')
 module.exports = class UpdateAccessTokenRepository {
   constructor (userModel) {
     this.userModel = userModel
   }
 
   async update (userId, accessToken) {
+    if (!userId) {
+      throw MissingParamError('userId')
+    }
+
+    if (!accessToken) {
+      throw MissingParamError('accessToken')
+    }
+
     const filter = { _id: userId }
     const set = { $set: { accessToken } }
 
